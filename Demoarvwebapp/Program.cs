05f9c849502e3f09b4941b93ddae35a8d123f15e
Arvind Singh <arvindsing@gmail.com>
2024-12-15 03:36:29 -0800

    demochanges1

--- Demoarvwebapp/Program.cs
+++ Demoarvwebapp/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
+//from demobranch
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -12,6 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
